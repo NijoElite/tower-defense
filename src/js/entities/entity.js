99f746c0148989909006b96acfffecd56f4be801
Eugene Malchenko <nijoelite@gmail.com>
2019-06-27 04:58:10 +0300

    Add onClick event to entity

--- src/js/entities/entity.js
+++ src/js/entities/entity.js
@@ -1,14 +1,50 @@
+const GameEvent = require('../lib/events');
+
 class Entity {
   constructor(opts = []) {
+    if (typeof opts.canvas === 'undefined') {
+      const err = new Error(`canvas is undefined`);
+      err.name = 'Invalid Argument';
+      throw err;
+    }
+
+    this._canvas = opts.canvas;
+    this._ctx = this._canvas.getContext('2d');
+
     this.frames = opts.frames || [];
     this.fps = opts.fps || 30;
     this.position = opts.position || {x: 0, y: 0};
+    this.size = opts.size || {width: 0, height: 0};
 
     this._events = new Map();
     this._timestamps = {
       lastShot: 0,
       lastAnimate: 0,
     };
+
+    // onHover(this)
+    this._events.set('onHover', new GameEvent('onHover'));
+    // onClick(this, )
+    this._events.set('onClick', new GameEvent('onClick'));
+
+    this._canvas.addEventListener('click', this._onClick.bind(this));
+    // this._canvas.addEventListener('click', this._onHover.bind(this));
+  }
+
+  _onClick(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const xInbound = (x > this.position.x) &&
+                     (x < this.position.x + this.size.width);
+
+    const yInbound = y > this.position.y &&
+                     y < this.position.y + this.size.height;
+
+    if (xInbound && yInbound) {
+      console.log(this);
+      this._fireEvent('onClick', this, e);
+    }
   }
 
   on(eventName, cb) {
@@ -41,8 +77,9 @@ class Entity {
     event.notifyAll(...args);
   }
 
-  draw(ctx) {
-    ctx.drawRect(thix.position.x, this.position.y, 5, 5);
+  draw() {
+    this._ctx.fillRect(this.position.x, this.position.y,
+        this.size.width, this.size.height);
   }
 
   update(progress) {
