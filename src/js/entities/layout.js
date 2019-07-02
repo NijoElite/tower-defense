b9a34c1b6e956dcf37fd3bde69fa304435d5c15b
Eugene Malchenko <nijoelite@gmail.com>
2019-07-02 07:47:18 +0300

    Update layout

--- src/js/entities/layout.js
+++ src/js/entities/layout.js
@@ -1,31 +1,42 @@
-// Breadth-first search in set
-function bfs(set, fn) {
+const Enemy = require('../entities/enemy');
+const Tower = require('../entities/tower');
+const GameMap = require('../entities/map');
+const Entity = require('../entities/entity');
+
+// Breadth-first search in entity
+function bfs(ent, fn) {
   const queue = [];
   const visitedNodes = [];
-  set.forEach((firstNode) => {
-    queue.push(firstNode);
-    while (queue.length !== 0) {
-      const node = queue.shift();
-      visitedNodes.push(node);
-      node.childs.forEach((child) => queue.push(child));
-      if (fn) {
-        fn(node);
-      }
+  queue.push(ent);
+  while (queue.length !== 0) {
+    const node = queue.shift();
+    visitedNodes.push(node);
+    node.childs.forEach((child) => queue.push(child));
+    if (fn) {
+      fn(node);
     }
-  });
+  };
 };
 
 
 class Layout {
-  constructor(canvas) {
+  constructor(canvas, map) {
     if (typeof canvas === 'undefined') {
       const err = new Error('Canvas is undefined');
       err.name = 'Invalid Argument';
       throw err;
     }
 
-    this._map = new Set();
-    this._menu = new Set();
+    if (!(map instanceof GameMap)) {
+      const err = new Error('Map is not instance of GameMap');
+      err.name = 'Invalid Argument';
+      throw err;
+    }
+
+    this._map = map;
+
+    this._container = new Entity();
+
     this._canvas = canvas;
     this._ctx = canvas.getContext('2d');
 
@@ -63,15 +74,47 @@ class Layout {
   draw() {
     this._ctx.clearRect(0, 0, 2000, 2000);
     bfs(this._map, (ent) => ent.draw(this._ctx));
-    bfs(this._menu, (ent) => ent.draw(this._ctx));
+    bfs(this._container, (ent) => ent.draw(this._ctx));
   }
 
-  addMenuEntity(entity) {
-    this._menu.add(entity);
+  addEntity(enemy) {
+    this._map.appendChild(enemy);
   }
 
-  addMapEntity(entity) {
-    this._map.add(entity);
+  addMenuItem(item) {
+    this._container.appendChild(item);
+  }
+
+  getEnemies() {
+    const result = [];
+
+    bfs(this._map, (ent) => {
+      if (ent instanceof Enemy) {
+        result.push(ent);
+      }
+    });
+
+    return result;
+  }
+
+  removeEntity(ent) {
+    this._map.removeChild(ent);
+  }
+
+  removeMenuItem(item) {
+    this._container.removeChild(item);
+  }
+
+  getTowers() {
+    const result = [];
+
+    bfs(this._map, (ent) => {
+      if (ent instanceof Tower) {
+        result.push(ent);
+      }
+    });
+
+    return result;
   }
 }
 
