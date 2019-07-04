a6fdd88ffe599824f80d90c645f255af2ed4419c
Eugene Malchenko <nijoelite@gmail.com>
2019-07-04 17:15:06 +0300

    Add Entity tests

--- test/entities/entity.test.js
+++ test/entities/entity.test.js
@@ -36,4 +36,67 @@ describe('Entity', () => {
       expect(() => entity.on('THIS_EVENT_DOES_NOT_EXIST', [])).to.throw();
     });
   });
+
+  describe('appendChild', () => {
+    let entity;
+    let child;
+
+    beforeEach(() => {
+      entity = new Entity();
+      child = new Entity();
+      entity.appendChild(child);
+    });
+
+    it('should append new child', () => {
+      expect(entity.childs.size).not.to.be.equal(0);
+    });
+
+    it('should delete child', () => {
+      entity.removeChild(child);
+      expect(entity.childs.size).to.be.equal(0);
+    });
+  });
+
+  describe('getAbsolutePosition()', () => {
+    let entity;
+
+    beforeEach(() => {
+      entity = new Entity({
+        position: {x: 0, y: 0},
+        size: {width: 30, height: 30},
+      });
+    });
+
+    afterEach(() => {
+      const position = entity.getAbsolutePosition();
+
+      expect(position.x).to.be.equal(0);
+      expect(position.y).to.be.equal(0);
+    });
+
+    it('should return top left corner position when ' +
+       'not centered and without parent', () => {
+      entity.centered = false;
+    });
+
+    it('should return top left corner position when ' +
+    'centered and without parent', () => {
+      entity.centered = true;
+    });
+
+    it('should return top left corner position when ' +
+    'centered, relative without parent', () => {
+      entity.centered = true;
+      entity.relative = true;
+    });
+
+    it('should return top left corner position when ' +
+    'centered, relative and with parent', () => {
+      entity.centered = true;
+      entity.relative = true;
+
+      entity.position = {x: -15, y: -15};
+      entity.parent = new Entity({position: {x: 15, y: 15}});
+    });
+  });
 });
