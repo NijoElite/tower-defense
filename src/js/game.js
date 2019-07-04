cfbf9be8694f5d67e5bd822146e0aae1ef4ad238
Eugene Malchenko <nijoelite@gmail.com>
2019-07-04 12:53:32 +0300

    Update game loop

--- src/js/game.js
+++ src/js/game.js
@@ -1,25 +1,22 @@
 const Tower = require('./entities/tower');
 const Enemy = require('./entities/enemy');
+const Place = require('./entities/place');
 const GameMap = require('./entities/map');
 const Layout = require('./entities/layout');
 const paths = require('./paths');
 const enemyFactory = require('./helpers/enemy-factory');
+const Entity = require('./entities/entity');
+
+const level = require('./levels/level-1');
 
 const canvas = document.getElementById('game-canvas');
 
 let lastRender = 0;
 let stop = false;
 
-const SPAWN_POINT = {x: 190, y: -30};
-
 class Game {
   constructor() {
-    this.towers = [];
-    this.enemies = [];
-
-    this.layout = new Layout(canvas);
-
-    this.map = new GameMap({
+    const map = new GameMap({
       names: ['map.jpg'],
       size: {
         width: 1600,
@@ -27,11 +24,147 @@ class Game {
       },
     });
 
-    this.layout.addMapEntity(this.map);
+    this.level = level;
+    this.layout = new Layout(canvas, map);
+
+    map.on('onClick', () => {
+      if (this._pause) return;
+      this.layout.closePopup();
+    });
+
     this.wave = 0;
     this.money = 5000;
     this.health = 5;
+    this._pause = false;
+
+    this._createMenu();
+    this._createPopups();
+    this.level.places.forEach((place) => {
+      place.on('onClick', this.placeClick.bind(this));
+      this.layout.addEntity(place);
+    });
   }
+
+
+  _createPopups() {
+    this._popups = {};
+
+    this._popups.pause = new Entity({
+      names: ['pause-bg.png'],
+      position: {x: 0, y: 0},
+      size: {width: 1600, height: 900},
+    });
+
+    const build = new Entity({
+      names: ['53.png'],
+      position: {x: 0, y: 0},
+      centered: true,
+      size: {width: 176, height: 170},
+    });
+
+    const buildFireTower = new Entity({
+      names: ['12.png'],
+      position: {x: -55, y: -60},
+      relative: true,
+      size: {width: 55, height: 60},
+      centered: true,
+    });
+    buildFireTower.type = 'fire';
+
+    const buildStoneTower = new Entity({
+      names: ['25.png'],
+      position: {x: 60, y: 60},
+      relative: true,
+      size: {width: 55, height: 60},
+      centered: true,
+    });
+    buildStoneTower.type = 'stone';
+
+    buildFireTower.on('onClick', this.buildClick.bind(this));
+    buildStoneTower.on('onClick', this.buildClick.bind(this));
+
+    build.appendChild(buildFireTower);
+    build.appendChild(buildStoneTower);
+
+    this._popups.build = build;
+  }
+
+  _createMenu() {
+    const startX = 1600;
+    const padding = 5;
+    const size = 32;
+
+    let count = 0;
+    const nextPos = () => {
+      count++;
+      return {
+        x: startX - size * count + (1 - count) * padding,
+        y: 0,
+      };
+    };
+
+    const pauseBtn = new Entity({
+      names: ['pause-btn.png'],
+      position: nextPos(),
+    });
+
+    pauseBtn.on('onClick', this.pauseClick.bind(this));
+
+    this.layout.addMenuItem(pauseBtn);
+  }
+
+  pauseClick(e) {
+    this._pause = !this._pause;
+
+    this.layout.showPopup(this._popups.pause);
+  }
+
+  placeClick(e) {
+    this._popups.build.position = {
+      x: e.position.x + 16,
+      y: e.position.y + 16,
+    };
+
+    this._popups.build.place = e;
+
+    this.layout.showPopup(this._popups.build);
+  }
+
+  buildClick(e) {
+    let names = [];
+    
+    const popup = e.parent;
+    const place = popup.place;
+
+    const pos = popup.getAbsolutePosition();
+    let damage = 0;
+    let cooldown = 0;
+
+    if (e.type === 'fire') {
+      names = ['12.png'];
+      damage = 3;
+      cooldown = 500;
+    }
+    if (e.type === 'stone') {
+      names = ['6.png'];
+      damage = 150;
+      cooldown = 3000;
+    }
+
+    const tower = new Tower({
+      names: names,
+      position: pos,
+      centered: true,
+      size: {width: 36, height: 50},
+      damage: damage,
+      cooldown: cooldown,
+    });
+
+    this.layout.removeEntity(place);
+    this.layout.addEntity(tower);
+    this.layout.closePopup();
+  }
+
 
   draw() {
     this.layout.draw();
@@ -39,76 +172,54 @@ class Game {
 
   start() {
     stop = false;
+
     this.generateEnemies();
-    this.fake();
     window.requestAnimationFrame(() => this._loop());
-  }
-
-
-  // 490, 233
-  fake() {
-    const tower = new Tower({
-      names: ['13.png'],
-      position: {
-        x: 490,
-        y: 233,
-      },
-    });
-
-    this.map.appendChild(tower);
-    this.towers.push(tower);
   }
 
   stop() {
     stop = true;
   }
 
-  _onEnemyFinished(enemy) {
-    this._removeEnemy(enemy);
+
+  onEnemyDeath(enemy) {
+    this.money += 50;
+
+    this.layout.removeEntity(enemy);
+    this.checkAliveEnemies();
+  }
+
+  onEnemyFinished(enemy) {
+    this.health -= 1;
+
+    this.layout.removeEntity(enemy);
     this.checkAliveEnemies();
   }
 
   checkAliveEnemies() {
-    if (this.enemies.length === 0) {
-      setTimeout(() => this.generateEnemies(), 3000);
+    const enemies = this.layout.getEnemies();
+    if (enemies.length === 0) {
+      setTimeout(this.generateEnemies.bind(this), 3000);
     }
-  }
-
-  _removeEnemy(enemy) {
-    this.map.removeChild(enemy);
-    this.enemies.splice(this.enemies.indexOf(enemy), 1);
-  }
-
-  _onEnemyDeath(enemy) {
-    this.money += 50;
-
-    this._removeEnemy(enemy);
-    this.checkAliveEnemies();
   }
 
   generateEnemies() {
-    const count = this.wave * 5 + 20;
+    const onDeath = this.onEnemyDeath.bind(this);
+    const onFinished = this.onEnemyFinished.bind(this);
 
-    for (let i = 0; i < count; i++) {
-      const xOffset = Math.random() * 50 - 25;
-      const yOffset = -Math.random() * 40 * i;
+    const enemies = this.level.generateEnemies(this.wave, onDeath, onFinished);
 
-      const spawn = {x: SPAWN_POINT.x + xOffset, y: SPAWN_POINT.y + yOffset};
-      const enemy = enemyFactory.random(spawn);
-      const path = paths[Math.trunc(Math.random() * paths.length)];
-
-      enemy.setPath(path);
-      enemy.on('onTarget', this._onEnemyFinished.bind(this));
-      enemy.on('onDeath', this._onEnemyDeath.bind(this));
-
-      this.enemies.push(enemy);
-      this.map.appendChild(enemy);
-    }
+    enemies.forEach((enemy) => {
+      this.layout.addEntity(enemy);
+    });
   }
 
   update(progress) {
-    this.enemies.forEach((enemy) => enemy.moveTo(progress));
-    this.towers.forEach((tower) => tower.attack(this.enemies));
+    const enemies = this.layout.getEnemies();
+    const towers = this.layout.getTowers();
+
+    enemies.forEach((enemy) => enemy.moveTo(progress));
+    towers.forEach((tower) => tower.attack(enemies));
   }
 
   _loop() {
@@ -116,12 +227,20 @@ class Game {
       lastRender = Date.now();
     }
 
-    const progress = Date.now() - lastRender;
-
-    this.update(progress);
     this.draw();
 
-    lastRender = Date.now();
+    if (this._pause) {
+      lastRender = Date.now();
+      window.requestAnimationFrame(this._loop.bind(this));
+      return;
+    }
+
+    const progress = Date.now() - lastRender;
+
+    if (progress > 1000 / 45) {
+      this.update(progress);
+      lastRender = Date.now();
+    }
 
     if (!stop) {
       window.requestAnimationFrame(this._loop.bind(this));
